524304bdc23972ecfdeb97fea979f3d9c8f6fb77
김승호 <naho199345@naver.com>
2022-01-26 22:57:17 +0900

    mongodb연결

--- schemas/index.js
+++ schemas/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connect = () => {
-    mongoose.connect('mongodb://test:test@54.180.147.13:27017/spa_article', { ignoreUndefined:true }).catch((err) => {
+    mongoose.connect('mongodb://test:test@54.180.147.13:27017/admin', { ignoreUndefined:true }).catch((err) => {
         console.error(err);
     })
 };
